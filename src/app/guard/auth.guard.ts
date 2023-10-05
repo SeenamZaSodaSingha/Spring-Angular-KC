9ce4f3a8177ae81b6e02d03682bc1acce63a3774
SeenamZaSodaSingha <thammanit.ch@gmail.com>
2023-10-05 14:09:11 +0700

    add  token expire checjk and kick

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -11,6 +11,7 @@ import {
 import { OAuthService } from 'angular-oauth2-oidc';
 import { authConfig } from '../auth.config';
 import { AuthService } from '../services/auth.service';
+import { TokenService } from '../services/token.service';
 
 
 @Injectable({
@@ -18,6 +19,7 @@ import { AuthService } from '../services/auth.service';
 })
 export class AuthGuard implements CanActivate {
   constructor(
+    private tokenService: TokenService,
     private authService: AuthService,
     private router: Router,
     private oauthService: OAuthService
@@ -31,18 +33,22 @@ export class AuthGuard implements CanActivate {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (this.authService.isAuthenticated()) {
+    const token = this.tokenService.getAccessToken();
+
+    console.log('Token Expire: ' + this.tokenService.isTokenExpired(token));
+
+    if (this.authService.isAuthenticated() && !this.tokenService.isTokenExpired(token)) {
       console.log(this.authService.isAuthenticated());
       console.log('User is authenticated');
-      return true; // User is authenticated, allow access
+      // if(this.tokenService.isTokenExpired(token)){
+      //   this.authService.login();
+      //   return false;
+      // }
+      return true;
     } else {
       console.log('User is not authenticated');
       console.log(this.authService.isAuthenticated());
       this.authService.login();
-      /**
-       * After login, the system instantely check the token and redirect to the login page again
-       * because of the pkce flow. So, we need to return false here and redirect to the login page
-       */
       return false;
     }
   }
--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TokenService {
+  constructor() {}
 
-  constructor() { }
+  // Retrieve the access token from local storage
+  getAccessToken(): string | null {
+    return sessionStorage.getItem('access_token');
+  }
+
+  // Check if a token is expired
+  isTokenExpired(token: string | null): boolean {
+    console.log('Toekn expire is called');
+    if (!token) {
+      console.log('Token is null');
+      return true; // Token is invalid or not present
+    }
+
+    const tokenData = this.parseToken(token);
+    if (!tokenData) {
+      console.log('Token is invalid');
+      return true; // Token is invalid or expired
+    }
+    // console.log('Token Expire: ' + tokenData.exp);
+    const currentTime = Math.floor(Date.now() / 1000); // Current time in seconds
+    console.log('Current Time: ' + currentTime);
+    console.log('Token Expire: ' + tokenData.exp);
+    return tokenData.exp < currentTime;
+  }
+
+  // Helper function to parse a JWT token and extract its payload
+  private parseToken(token: string): any | null {
+    try {
+      const tokenParts = token.split('.');
+      if (tokenParts.length !== 3) {
+        return null;
+      }
+      const payload = JSON.parse(atob(tokenParts[1]));
+      if (!payload.exp) {
+        return null;
+      }
+      return payload;
+    } catch (error) {
+      return null;
+    }
+  }
 }
